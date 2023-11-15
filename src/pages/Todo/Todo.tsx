cfb649f7916478a3705648162b982c2a5626c0ce
yeon <yeonnnnn.k@gmail.com>
2023-11-15 18:27:46 +0900

    🐣 ADD: Todo페이지 UI 구현

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -5,7 +5,23 @@ import { S } from "./Todo";
 const Todo = () => {
   return (
     <S.Todo>
-      <div></div>
+      <S.BgImg>
+        <S.Brightness>
+          <S.CatchPhraseBox>
+            <S.UpperCatchPhrase>Todo</S.UpperCatchPhrase>
+            <S.LowerCatchPhrase>Dare to dream, Dare to do</S.LowerCatchPhrase>
+          </S.CatchPhraseBox>
+          <S.ExceptCatchPhrase>
+            <S.LeftBox>
+              <S.WheaterBox></S.WheaterBox>
+              <S.CalanderBox></S.CalanderBox>
+            </S.LeftBox>
+            <S.RightBox>
+              <S.ATodo></S.ATodo>
+            </S.RightBox>
+          </S.ExceptCatchPhrase>
+        </S.Brightness>
+      </S.BgImg>
     </S.Todo>
   );
 };
